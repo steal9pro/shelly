315f48f3bcd55315eecfcf1885eefdfd65401a80
Prokhorovych Andrii <prohorovychua@gmail.com>
2025-01-16 21:59:04 +0200

    feat: add exit cmd

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #[allow(unused_imports)]
+use std::process;
 use std::io::{self, Write};
 
 fn main() {
@@ -13,6 +14,12 @@ fn run_loop() {
         let mut input = String::new();
 
         wait_input(&mut input);
+        let (cmd, args) = parse_command(&input);
+
+        if cmd == "exit" {
+            process::exit(0x0100);
+        }
+
         not_found(input)
     }
 }
@@ -21,6 +28,16 @@ fn wait_input(input: &mut String) {
     // Wait for user input
     let stdin = io::stdin();
     stdin.read_line(input).unwrap();
+}
+
+fn parse_command(input: &String) -> (&str, &str) {
+   let mut parts = input.split(" "); 
+
+   let cmd = parts.next().unwrap();
+
+   let arg = parts.next().unwrap_or("");
+
+   return (cmd, arg)
 }
 
 fn not_found(cmd_name: String) {
